cfa6e477cdcdfbb86a8f0d70b55e4ca60f8b00e7
Adam Pearce <1wheel@gmail.com>
2013-02-16 23:41:55 -0600

    update

---
+++ backbone/todo/example.js
@@ -0,0 +1,19 @@
+var TodoRouter = Backbone.Router.extend({
+  /* define the route and function maps for this router */
+  routes: {
+    "about" : "showAbout",
+    "search/:query" : "searchTodos",
+    "search/:query/p:page" : "searchTodos"
+  },
+
+  showAbout: function(){},
+
+  searchTodos: function(query, page){
+    var page_number = page || 1;
+    console.log("Page number: " + page_number + " of the results for todos containing the word: " + query);
+  }
+});
+
+var myTodoRouter = new TodoRouter();
+
+Backbone.history.start();
