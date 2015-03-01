e8ead2f0ddc5d86fb55e1d71d6494543b79b83cb
Adam Pearce <1wheel@gmail.com>
2015-03-01 02:30:51 -0500

    hw 4

--- stat545/dpylr/script.r
+++ stat545/dpylr/script.r
@@ -1,10 +1,4 @@
-
-GDPextent <- gtbl %>%
-  select(year, country, continent, gdpPercap) %>%
-  arrange(continent, year) %>%
-  group_by(continent) %>%
-  filter(min_rank(desc(gdpPercap)) < 2 | min_rank(gdpPercap) < 2)
-
-(GDPextent)
-
-ggplot(GDPextent, aes(x = year, y = gdpPercap, color=continent)) + geom_point() +geom_line()
+gtbl  <-  gtbl %>%
+  mutate(bench = meanByYear$lifeExp[match(year, meanByYear$year)]
+         benchDif = lifeExp - bench
+         belowBench = lifeExp < bench)
