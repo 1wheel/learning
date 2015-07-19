4475c06824a974e7181c12a238593fb1e01a4afa
Adam Pearce <1wheel@gmail.com>
2015-07-19 13:03:34 -0400

    q2

---
+++ rprog/getdata-q2/oauth2.R
@@ -0,0 +1,28 @@
+library(httr)
+
+# 1. Find OAuth settings for github:
+#    http://developer.github.com/v3/oauth/
+oauth_endpoints("github")
+
+# 2. To make your own application, register at at
+#    https://github.com/settings/applications. Use any URL for the homepage URL
+#    (http://github.com is fine) and  http://localhost:1410 as the callback url
+#
+#    Replace your key and secret below.
+myapp <- oauth_app("github",
+                   key = "40a89b5cef2ba740a2aa",
+                   secret = "91f9ecc1c6076d8c32fca9239a228250dc04b91c")
+
+# 3. Get OAuth credentials
+github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
+
+# 4. Use API
+gtoken <- config(token = github_token)
+req <- GET("https://api.github.com/rate_limit", gtoken)
+stop_for_status(req)
+content(req)
+
+# OR:
+req <- with_config(gtoken, GET("https://api.github.com/rate_limit"))
+stop_for_status(req)
+content(req)
