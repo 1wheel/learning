f1f7d98224fa78d2c3636d4cb885508bbf7b8c48
Adam Pearce <1wheel@gmail.com>
2015-08-03 23:59:33 -0400

    starting!

---
+++ clojure-scip/src/clojure_scip/core.clj
@@ -0,0 +1,21 @@
+(ns clojure-scip.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
+
+(println 23123)
+(println 23123)
+(println 23123)
+
+(let [my-var (+ 1
+                2
+                3)])
+
+(defn my-fn [x y z] x)
+
+(count (my-fn "hello" 3 4))
+
+234234
---
+++ clojure-scip/test/clojure_scip/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-scip.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-scip.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
